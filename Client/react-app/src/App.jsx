ee30f5a6d1ad045b2faf8c128b02490fa29351f2
Shubhanker01 <shubhanker40@gmail.com>
2024-07-22 20:39:19 +0530

    commit3

--- Client/react-app/src/App.jsx
+++ Client/react-app/src/App.jsx
@@ -1,11 +1,19 @@
 import Signup from "./components/Signup"
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import "../src/App.css"
+import Login from "./components/Login"
 function App() {
   
 
   return (
     <>
-        <Signup/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </Router>
+        
     </>
   )
 }
--- Client/react-app/src/components/Login.jsx
+++ Client/react-app/src/components/Login.jsx
@@ -1,22 +1,10 @@
 import { Link } from "react-router-dom"
-
-const Signup = () => {
+const Login = () => {
     return (
         <>
             <div className="signup-form">
                 <form>
-                    <h1>SignUp Form</h1>
-                    <div>
-                        <label>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                            </svg>
-                        </label>
-                        <input type="text" placeholder="Your Name" id="username" required></input>
-                    </div>
-
-
-                    <br></br>
+                    <h1>Login Form</h1>
                     <div>
                         <label>
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
@@ -36,22 +24,12 @@ const Signup = () => {
                         <input type="password" placeholder="Your Password" id="password" required></input>
                     </div>
                     <br></br>
-                    <div>
-                        <label>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
-                                <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2M2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
-                            </svg>
-                        </label>
-                        <input type="password" placeholder="Confirm Password" id="confirm-password" required></input>
-                    </div>
-
+                    <button type="submit">Login</button>
                     <br></br>
-                    <button type="submit">Register</button>
-                    <br></br>
-                    <p>Already Registered? <Link to="/login">Login</Link></p>
+                    <p>New User? Go To <Link to="/">Registration</Link></p>
                 </form>
             </div>
         </>
     )
 }
-export default Signup
+export default Login
