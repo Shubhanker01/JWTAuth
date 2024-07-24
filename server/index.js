353c91437d8d771052a592e3c9a6f1ad84de3478
Shubhanker01 <shubhanker40@gmail.com>
2024-07-24 17:50:52 +0530

    commit2

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
+
 const port = 3000
 app.use(cors())
-app.use(bodyParser.urlencoded({extended:false}))
-app.post('/signup', (req, res) => {
-    res.send("successfully submitted")
-})
+app.use(bodyParser.urlencoded({ extended: false }))
 
+app.post('/signup', function (req, res) {
+    try {
+        let token = jwt.sign(req.body,"shhhh")
+        res.send(token)
+    } catch (error) {
+        console.log(error)
+    }
+})
 app.listen(port, () => {
     console.log(`App is listening on port:${port}`)
 })
